82413f370afc65eb3e8427a88e20086ae9e8cf59
julianabvilela <juliana.bragavilela@gmail.com>
2024-11-13 13:10:36 -0600

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -4,8 +4,3 @@ frase = input('Digite uma frase: ')
 print('A frase digitada foi: {}' .format(frase))
 invertida = ' '.join(palavra[::-1] for palavra in frase.split())
 print('A frase de modo inevertida fica: {}'.format(invertida))
-
-
-
-elif (a=b) and (a=c) and (b=a) and (b=c) and (c=a) and (c=b):
-    print("Forma um triangulo equilatero")
